61ad13770b6c52cc06626f4bf0017122f31ddb3e
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-17 15:26:03 -0800

    first blogSelector test passing

---
+++ src/selectors/blogSelector.test.js
@@ -0,0 +1,13 @@
+import { getBlogs, countBlogs } from './blogSelectors';
+
+describe('test selectors', () => {
+  it('gets blogs from state', () => {
+    const state = {
+      blogs: [{ title: 'title', body: 'body' }],
+    };
+
+    const selector = getBlogs(state);
+
+    expect(selector).toEqual([{ title: 'title', body: 'body' }]);
+  });
+});
