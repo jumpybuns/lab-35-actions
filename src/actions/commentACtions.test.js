3f50af08245d734eb8af962b300ee286333815b4
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-18 13:41:23 -0800

    add commentActions.test

---
+++ src/actions/commentACtions.test.js
@@ -0,0 +1,26 @@
+import {
+  setComment,
+  SET_COMMENT,
+  deleteComment,
+  DELETE_COMMENT,
+} from './commentActions';
+
+describe('comment actions', () => {
+  it('creates a comment using SET_COMMENT', () => {
+    const action = setComment(0, 'comment');
+
+    expect(action).toEqual({
+      type: SET_COMMENT,
+      payload: { index: 0, comment: 'comment' },
+    });
+  });
+
+  it('deletes a comment using DELETE_COMMENT', () => {
+    const action = deleteComment(0, 'comment');
+
+    expect(action).toEqual({
+      type: DELETE_COMMENT,
+      payload: { index: 0, comment: 'comment' },
+    });
+  });
+});
