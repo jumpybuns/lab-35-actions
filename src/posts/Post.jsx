62e769dbd5ec17969e6f4aac292dda5dcf113314
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-19 11:16:43 -0800

    reconfig Post.jsx

--- src/posts/Post.jsx
+++ src/posts/Post.jsx
@@ -1,51 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { deletePost } from '../actions/postActions';
-import { createComment } from '../actions/commentActions';
 
-const Post = ({ title, body, comments, index }) => {
+const Post = ({ title, body, postId }) => {
   const dispatch = useDispatch();
-  const [selectedComment, setSelectedComment] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(createComment(index, selectedComment));
-  };
 
   const handleClick = (e) => {
     e.preventDefault();
 
-    dispatch(deletePost(title));
+    dispatch(deletePost(postId));
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <p>{title}</p>
-      {comments.map((comment) => (
-        <>
-          <input
-            id={comments}
-            name="comments"
-            type="text"
-            value={comment}
-            onChange={({ target }) => setSelectedComment(target.value)}
-          />
-          <h1>{title}</h1>
-          <h2>{body}</h2>
-          <button onClick={handleClick}>Delete</button>
-          <label htmlFor={comment}>{comment}</label>
-        </>
-      ))}
-    </form>
+    <>
+      <dl>
+        <dt>{title}</dt>
+        <dd>{body}</dd>
+        <button onClick={handleClick}>Delete</button>
+      </dl>
+    </>
   );
 };
 
 Post.propTypes = {
   title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
-  comments: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-  index: PropTypes.number.isRequired,
+  postId: PropTypes.number.isRequired,
 };
 
 export default Post;
