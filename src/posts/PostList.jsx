3d1f05105591d32cddbeb71fc5feac0139acc2dd
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-19 11:18:52 -0800

    reconfig PostList.jsx

--- src/posts/PostList.jsx
+++ src/posts/PostList.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { getPosts } from '../selectors/postSelectors';
 import Post from './Post';
+import { Link } from 'react-router-dom';
 
 const PostList = () => {
   const posts = useSelector(getPosts);
 
-  const postElements = posts.map((post, i) => (
-    <li key={post.title}>
-      <Post {...post} index={i} />
+  const postElements = posts.map((post, index) => (
+    <li key={index}>
+      <Link to={`/post/${post.postId}`}>
+        <Post {...post} />
+      </Link>
     </li>
   ));
 
